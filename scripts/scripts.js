47a81b902da8ced579b635b279997a2a6153ba25
Guillermo <guillermoc93@protonmail.com>
2021-06-13 09:40:15 -0500

    working addBookToLibrary function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,5 @@
+let myLibrary = [];
+
 function Book (title, author, pages, read) {
   this.title = title
   this.author = author
@@ -6,4 +8,13 @@ function Book (title, author, pages, read) {
   this.info = function () {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}.`
   }
+}
+
+function addBookToLibrary() {
+  let title = prompt("Please enter the book title")
+  let author = prompt("Please enter book author")
+  let pages = prompt("Please enter the number of pages in the book")
+  let read = prompt("Have you read this book?")
+  myLibrary.push(new Book(title, author, pages, read))
+  return
 }
