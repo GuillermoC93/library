9b6f387f774d18656d5d171e6d746c0070642e5e
Guillermo <guillermoc93@protonmail.com>
2021-06-14 09:28:37 -0500

    display for library array working fine

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,23 @@
-let myLibrary = [];
+let myLibrary = [
+  {
+    title: "harry potter",
+    author: "jk rowling",
+    pages: 255,
+    read: "yes",
+  },
+  {
+    title: "game of thrones",
+    author: "grr martin",
+    pages: 523,
+    read: "yes",
+  },
+  {
+    title: "atomic habits",
+    author: "james clear",
+    pages: 244,
+    read: "no",
+  }
+];
 
 function Book (title, author, pages, read) {
   this.title = title
@@ -18,3 +37,35 @@ function addBookToLibrary() {
   myLibrary.push(new Book(title, author, pages, read))
   return
 }
+
+function displayBooks() {
+  const container = document.querySelector(".container");
+
+  myLibrary.forEach(book => {
+    const bookdiv = document.createElement("div");
+    const titlediv = document.createElement("div");
+    const authordiv = document.createElement("div");
+    const pagesdiv = document.createElement("div");
+    const readdiv = document.createElement("div");
+
+    titlediv.textContent = book.title
+    authordiv.textContent = book.author
+    pagesdiv.textContent = book.pages
+    readdiv.textContent = book.read
+
+    bookdiv.classList.toggle("book")
+    titlediv.classList.toggle("booktitle")
+    authordiv.classList.toggle("bookauthor")
+    pagesdiv.classList.toggle("bookpages")
+    readdiv.classList.toggle("bookread")
+
+    bookdiv.appendChild(titlediv)
+    bookdiv.appendChild(authordiv)
+    bookdiv.appendChild(pagesdiv)
+    bookdiv.appendChild(readdiv)
+
+    container.appendChild(bookdiv)
+  })
+}
+
+displayBooks()
